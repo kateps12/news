91b934a792d683ee00d616ff587c9a5f5eb38bf3
kateps12 <kateps12@gmail.com>
2020-02-19 03:03:21 +0000

    start hmwk

--- app.rb
+++ app.rb
@@ -7,12 +7,26 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
 # enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "34a5c5394a5b9ab52c00af35bd1959e1"
 
 get "/" do
   # show a view that asks for the location
+
+@lat = rand(-90.0..90.0)  
+@long = rand(-180.0..180.0)
+@lat_long = "#{@lat},#{@long}" 
+
+results = Geocoder.search("Evanston, IL")
+results.first.coordinates #=> [42.0574063,-87.6722787]
+
 end
 
 get "/news" do
   # do everything else
+
+  #news API code: eca1d8895efa4e26811dece4f7ff13c6
+url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=eca1d8895efa4e26811dece4f7ff13c6"
+news = HTTParty.get(url).parsed_response.to_hash
+# news is now a Hash you can pretty print (pp) and parse for your output
+
 end
